8f6da5ef7daede6d69cff1b358cf55018de257b2
inceabdullah <pax.castrensis@gmail.com>
2019-08-03 02:38:41 -0700

    Update get_topimgurl_from_news.py

--- get_topimgurl_from_news.py
+++ get_topimgurl_from_news.py
@@ -2,11 +2,17 @@ from newspaper import Article
 
 # get_topimgurl_from_news.get_topimgurl_from_news(url)
 
+# default haber resmi: http://haber.ozguruygulama.com/static/h_news.png
+
 def get_topimgurl_from_news(url):
     article = Article(url)
     article.download()
-    article.parse()
+    try:
+        article.parse()
+        image_url = article.top_image
+    except:
+        image_url = "http://haber.ozguruygulama.com/static/h_news.png"
     
-    image_url = article.top_image
+    
     
     return image_url
